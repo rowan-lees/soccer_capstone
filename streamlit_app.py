4d03cda6c32b181694a64a09c110e4ba94a414bd
rowan-lees <rowan-lees@hotmail.com>
2023-04-28 18:36:41 -0700

    changed error message

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,17 +39,17 @@ sample_match = match_df.sample(n=1)
 
 st.title("Applying Machine Learning for Soccer Betting Success")
 
+st.write(f"{match_df['home_team_name'].values[0]} vs. {match_df['away_team_name'].values[0]}")
+st.write(f"Date: {match_df['date'].dt.strftime('%d-%m-%Y').values[0]}")
+
 wager_str = st.text_input("Enter your $$ wager")
 
 try:
     wager = float(wager_str)
 except ValueError:
-    st.error("Please enter a valid numeric wager, dollar sign should be excluded")
+    st.warning("Please enter a valid wager (e.g. 100 or 55.55), please exclude the dollar sign")
 
 st.write("Wager entered:", wager)
-
-st.write(f"{match_df['home_team_name'].values[0]} vs. {match_df['away_team_name'].values[0]}")
-st.write(f"Date: {match_df['date'].dt.strftime('%d-%m-%Y').values[0]}")
 
 home_w_button = st.button("Home Team Win")
 draw_button = st.button("Draw")
