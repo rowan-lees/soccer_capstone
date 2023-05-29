1fc1d91fabf5627860394dc31caffe6dc7e4fe09
rowan-lees <rowan-lees@hotmail.com>
2023-05-28 18:36:37 -0700

    small changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,12 @@ import pandas as pd
 from pandas import Timestamp
 import random
 
+# Read in data from the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
 
 match_dict = {2766: {'date': Timestamp('2011-04-24 00:00:00'),
   'home_team_name': 'Bolton Wanderers',
