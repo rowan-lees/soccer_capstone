ec083d97f145722b1e14194a22a3394bf8066749
rowan-lees <rowan-lees@hotmail.com>
2023-05-31 21:19:43 -0700

    changed to multipage app

---
+++ Hello.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to my Streamlit App! 👋")
+st.sidebar.success("Select a option")
+
+st.markdown(
+    """
+    This app does blah blah blah
+    """
+)
