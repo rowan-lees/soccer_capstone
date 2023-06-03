571ce588add08ecd6c420b75d27c22d5ee63d843
rowan-lees <rowan-lees@hotmail.com>
2023-06-03 15:09:23 -0700

    flag adj

--- pages/1_💰_Betting_App.py
+++ pages/1_💰_Betting_App.py
@@ -16,25 +16,29 @@ def load_data(sheets_url):
     return pd.read_csv(csv_url)
 
 Country_league_flag = load_data(st.secrets["Country_league_flag_url"])
-# country_list = load_data(st.secrets["country_list_url"])
 league_table = load_data(st.secrets["league_table_url"])
 test_matches = load_data(st.secrets["Test_matches_url"])
 match_data = load_data(st.secrets["match_data_url"])
 
 sample = test_matches.sample(1)
+samp_season = sample['season'].values[0]
+samp_league = sample['League'].values[0]
+samp_stage = sample['stage'].values[0]
+samp_country = sample['country'].values[0]
+samp_h_team = sample["home_team_name"].values[0]
+samp_a_team = sample["away_team_name"].values[0]
 
 # filt_leag_8_9_England = league_table[(league_table['country']=='England') & (league_table['season']=='2009/2010')]
 # st.dataframe(filt_leag_8_9_England)\
 
-st.markdown(f'<h1 style="color: #2C74D3;">{sample["home_team_name"].values[0]}  \
-            vs  {sample["away_team_name"].values[0]}</h1>', unsafe_allow_html=True)
+st.markdown(f'<h1 style="color: #2C74D3;">{samp_h_team}  vs  {samp_a_team}</h1>', unsafe_allow_html=True)
 
-flag_url = Country_league_flag[Country_league_flag['League'] == (sample['League'].values[0])]['URL'].values[0]
+flag_url = Country_league_flag[Country_league_flag['League'] == (samp_league)]['URL'].values[0]
 
-st.image(flag_url, width=50)
-st.write(f"League: {sample['League'].values[0]}")
-st.write(f"Season: {sample['season'].values[0]}")
-st.write(f"Match Day #{sample['stage'].values[0]}")
+st.image(flag_url, caption={samp_country}, width=50,output_format='JPEG',style='border: 1px solid white;')
+st.write(f"League: {samp_league}")
+st.write(f"Season: {samp_season}")
+st.write(f"Match Day #{samp_stage}")
 
 
 season_list = ['ALL',
@@ -47,9 +51,7 @@ season_list = ['ALL',
  '2014/2015',
  '2015/2016']
 
-samp_season = sample['season'].values[0]
-samp_league = sample['League'].values[0]
-samp_stage = sample['stage'].values[0]
+
 
 
 current_table = filtered_table(match_data, samp_season, samp_league, samp_stage)
