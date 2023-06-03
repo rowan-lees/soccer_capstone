4bee35e7fb27203e76bdabe795aef79f88dea958
rowan-lees <rowan-lees@hotmail.com>
2023-06-02 17:09:26 -0700

    adding assert statements

--- func_filt_league.py
+++ func_filt_league.py
@@ -4,10 +4,15 @@
 from func_master_league_table import f_league_table
 
 def filtered_table(match_data, season, league, stage):
+    
+    assert isinstance(season, str), "Season must be a string"
+    assert isinstance(league, str), "League must be a string"
+    assert isinstance(stage, int), "Stage must be an integer"
+    
     filtered = match_data[(match_data["season"] == season) \
                 & (match_data["League"] == league) \
                 & (match_data["stage"] < stage)]
 
     filtered_league_table = f_league_table(filtered)
 
-    return filtered_league_table
+    return filtered_league_table 
