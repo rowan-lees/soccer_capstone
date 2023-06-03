e95bd16aaca39f8fa8a45ed4c9c2664a50331a4d
rowan-lees <rowan-lees@hotmail.com>
2023-06-03 14:55:57 -0700

    add flag table

--- pages/1_💰_Betting_App.py
+++ pages/1_💰_Betting_App.py
@@ -15,8 +15,8 @@ def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit?usp=sharing", "/export?format=csv")
     return pd.read_csv(csv_url)
 
-league_list = load_data(st.secrets["league_list_url"])
-country_list = load_data(st.secrets["country_list_url"])
+Country_league_flag = load_data(st.secrets["Country_league_flag_url"])
+# country_list = load_data(st.secrets["country_list_url"])
 league_table = load_data(st.secrets["league_table_url"])
 test_matches = load_data(st.secrets["Test_matches_url"])
 match_data = load_data(st.secrets["match_data_url"])
@@ -29,8 +29,9 @@ filt_leag_8_9_England = league_table[(league_table['country']=='England') & (lea
 st.markdown(f'<h1 style="color: #2C74D3;">{sample["home_team_name"].values[0]}  \
             vs  {sample["away_team_name"].values[0]}</h1>', unsafe_allow_html=True)
 
-st.image("https://creazilla-store.fra1.digitaloceanspaces.com/emojis/55698/england-flag-emoji-clipart-xl.png",\
-        width=50)
+flag_url = Country_league_flag[Country_league_flag['League'] == (sample['League'].values[0])]['URL'].values[0]
+
+st.image(flag_url, width=50)
 st.write(f"League: {sample['League'].values[0]}")
 st.write(f"Season: {sample['season'].values[0]}")
 st.write(f"Match Day #{sample['stage'].values[0]}")
@@ -55,22 +56,22 @@ current_table = filtered_table(match_data, samp_season, samp_league, samp_stage)
 
 st.dataframe(current_table)
 
-league_selection = st.selectbox("Select League",league_list)
-season_selection = st.selectbox("Select Season",season_list)
+# league_selection = st.selectbox("Select League",league_list)
+# season_selection = st.selectbox("Select Season",season_list)
 
-def generate_random_match():
-    match_id = random.choice(list(match_dict.keys()))
-    match_info = match_dict[match_id]
-    match_df = pd.DataFrame.from_dict({match_id:match_info}, orient='index')
-    return match_df
+# def generate_random_match():
+#     match_id = random.choice(list(match_dict.keys()))
+#     match_info = match_dict[match_id]
+#     match_df = pd.DataFrame.from_dict({match_id:match_info}, orient='index')
+#     return match_df
 
-match_generated = False
-sample_match = None
+# match_generated = False
+# sample_match = None
 
-# Generate a new match
-if st.button("Generate New Match"):
-    sample_match = generate_random_match()
-    match_generated = True
+# # Generate a new match
+# if st.button("Generate New Match"):
+#     sample_match = generate_random_match()
+#     match_generated = True
 
 wager_str = st.text_input("Enter your $$ wager")
 
@@ -81,24 +82,24 @@ except ValueError:
 
 st.write("Wager entered:", wager_str)
 
-if match_generated:
-    # Display the match information
-    st.write(f"{sample_match['home_team_name'].values[0]} vs. {sample_match['away_team_name'].values[0]}")
-    st.write(f"Date: {sample_match['date'].dt.strftime('%d-%m-%Y')}")
+# if match_generated:
+#     # Display the match information
+#     st.write(f"{sample_match['home_team_name'].values[0]} vs. {sample_match['away_team_name'].values[0]}")
+#     st.write(f"Date: {sample_match['date'].dt.strftime('%d-%m-%Y')}")
 
-    # Get the user's bet
-    home_w_button = st.button("Home Team Win")
-    draw_button = st.button("Draw")
-    away_w_button = st.button("Away Team Win")
+#     # Get the user's bet
+#     home_w_button = st.button("Home Team Win")
+#     draw_button = st.button("Draw")
+#     away_w_button = st.button("Away Team Win")
 
-    if home_w_button:
-        st.write(f"You wagered ${wager} that {sample_match['home_team_name'].values[0]} will Win!")
-    elif away_w_button:
-        st.write(f"You wagered ${wager} that {sample_match['away_team_name'].values[0]} will Win!")
-    elif draw_button:
-        st.write(f"You wagered ${wager} that the match will end in a Draw!")
-    else:
-        st.write("Please make a selection.")
-else:
-    st.write("Please generate a match.") 
+#     if home_w_button:
+#         st.write(f"You wagered ${wager} that {sample_match['home_team_name'].values[0]} will Win!")
+#     elif away_w_button:
+#         st.write(f"You wagered ${wager} that {sample_match['away_team_name'].values[0]} will Win!")
+#     elif draw_button:
+#         st.write(f"You wagered ${wager} that the match will end in a Draw!")
+#     else:
+#         st.write("Please make a selection.")
+# else:
+#     st.write("Please generate a match.") 
 
