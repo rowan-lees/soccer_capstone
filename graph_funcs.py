42e7454a269d39942e8cd2f5a9b09c5575322074
rowan-lees <rowan-lees@hotmail.com>
2023-06-14 17:51:54 -0700

    graph func

--- graph_funcs.py
+++ graph_funcs.py
@@ -13,11 +13,13 @@ def five_match_line_plt(match_data, league, season, stage, h_team, a_team):
         teams = [h_team, a_team]
 
         home_form = current_season[['stage', 'h_five_form_pts', 'home_team_name']].rename(columns={'h_five_form_pts':'form_pts', 'home_team_name':'team_name'})
-        away_form = current_season[['stage', 'a_five_form_pts', 'away_team_name']].rename(columns={'a_five_form_pts': 'form_pts', 'away_team_name': 'team_name'})
+        away_form = current_season[['stage', 'a_five_form_pts', 'away_team_name']].rename(columns={'a_five_form_pts':'form_pts', 'away_team_name':'team_name'})
 
         form_melt = pd.concat([home_form,away_form]).sort_values(by=['team_name', 'stage'])
         form_melt = form_melt[form_melt['team_name'].isin(teams)]
         form_melt['form_avg_pts'] = form_melt['form_pts'] / 5
+
+        form_melt.head()
 
         plt.figure(figsize=(8,6))
         for team in teams:
@@ -26,7 +28,7 @@ def five_match_line_plt(match_data, league, season, stage, h_team, a_team):
             plt.plot(team_data_smooth[:,0], team_data_smooth[:,1], label=team, alpha=0.7)
 
         plt.legend(bbox_to_anchor=(1.05, 1))
-        plt.title("5 Match Form for Home and Away team\nWith Lowess smoothing applied")
+        plt.title("5 Match Form for Home and Away team")
         plt.ylabel("Average Points Accumluted over Past 5 Games")
         plt.xlabel(f"Match Day of {season} Season")
         plt.show()
