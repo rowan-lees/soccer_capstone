2d2e3c4939abfcc69ce2f587ae1dd7dbb65054ed
rowan-lees <rowan-lees@hotmail.com>
2023-06-23 20:09:00 -0700

    .

--- pages/1_💰_Betting_App.py
+++ pages/1_💰_Betting_App.py
@@ -391,14 +391,14 @@ if wager_str:
                 st.session_state.model_pred_running_total += pred_winnings
                 if st.session_state.model_pred_running_total > 0:
                     if pred_winnings > 0:
-                        st.write(f"Model Prediction Running Total: <span style='color:green; font-size: 18px'>${round(st.session_state.model_pred_running_total,2)}<span style='color:green; font-size: 12px'>&nbsp;&nbsp;(${round(pred_winnings,2)})</span></span>", unsafe_allow_html=True)
+                        st.write(f"Model Prediction Running Total: <span style='color:green; font-size: 18px'>${round(st.session_state.model_pred_running_total,2)}</span><span style='color:green; font-size: 12px'>&nbsp;&nbsp;(${round(pred_winnings,2)})</span>", unsafe_allow_html=True)
                     else:
-                        st.write(f"Model Prediction Running Total: <span style='color:green; font-size: 18px'>${round(st.session_state.model_pred_running_total,2)}<span style='color:red; font-size: 12px'>&nbsp;&nbsp;(${round(pred_winnings,2)})</span></span>", unsafe_allow_html=True)
+                        st.write(f"Model Prediction Running Total: <span style='color:green; font-size: 18px'>${round(st.session_state.model_pred_running_total,2)}</span><span style='color:red; font-size: 12px'>&nbsp;&nbsp;(${round(pred_winnings,2)})</span>", unsafe_allow_html=True)
                 else:
                     if pred_winnings <= 0:
-                        st.write(f"Model Prediction Running Total: <span style='color:red; font-size: 18px'>${round(st.session_state.model_pred_running_total,2)}<span style='color:green; font-size: 12px'>&nbsp;&nbsp;(${round(pred_winnings,2)})</span></span>", unsafe_allow_html=True)
+                        st.write(f"Model Prediction Running Total: <span style='color:red; font-size: 18px'>${round(st.session_state.model_pred_running_total,2)}</span><span style='color:green; font-size: 12px'>&nbsp;&nbsp;(${round(pred_winnings,2)})</span>", unsafe_allow_html=True)
                     else:
-                        st.write(f"Model Prediction Running Total: <span style='color:red; font-size: 18px'>${round(st.session_state.model_pred_running_total,2)}<span style='color:red; font-size: 12px'>&nbsp;&nbsp;(${round(pred_winnings,2)})</span></span>", unsafe_allow_html=True)
+                        st.write(f"Model Prediction Running Total: <span style='color:red; font-size: 18px'>${round(st.session_state.model_pred_running_total,2)}</span><span style='color:red; font-size: 12px'>&nbsp;&nbsp;(${round(pred_winnings,2)})</span>", unsafe_allow_html=True)
     
     except ValueError:
         st.warning("Please enter a valid wager (e.g. 100 or 55.55), excluding the dollar sign.")
