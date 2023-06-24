f42f65e27dc638dbe6d62ce90e9ba137714e521e
rowan-lees <rowan-lees@hotmail.com>
2023-06-23 20:35:36 -0700

    .

--- pages/1_💰_Betting_App.py
+++ pages/1_💰_Betting_App.py
@@ -383,48 +383,13 @@ if wager_str:
 
 
 
-                # st.session_state.model_pred_running_total += pred_winnings
-                # if st.session_state.model_pred_running_total > 0:
-                #     st.write(f"Model Prediction Running Total: <span style='color:green'>${round(st.session_state.model_pred_running_total,2)}</span>", unsafe_allow_html=True)
-                # else:
-                #     st.write(f"Model Prediction Running Total: <span style='color:red'>${round(st.session_state.model_pred_running_total,2)}</span>", unsafe_allow_html=True)
-
                 st.session_state.model_pred_running_total += pred_winnings
                 if st.session_state.model_pred_running_total > 0:
-                    if pred_winnings > 0:
-                        st.write(f"Model Prediction Running Total: :green[${round(st.session_state.model_pred_running_total,2)}] :green[$+{round(pred_winnings,2)}]")
-                    else:
-                        st.write(f"Model Prediction Running Total: :green[${round(st.session_state.model_pred_running_total,2)}] :red[${round(pred_winnings,2)}]")
+                    st.write(f"Model Prediction Running Total: <span style='color:green'>${round(st.session_state.model_pred_running_total,2)}</span>", unsafe_allow_html=True)
                 else:
-                    if pred_winnings > 0:
-                        st.write(f"Model Prediction Running Total: :red[${round(st.session_state.model_pred_running_total,2)}] :green[${round(pred_winnings,2)}]")
-                    else:
-                        st.write(f"Model Prediction Running Total: :red[${round(st.session_state.model_pred_running_total,2)}] :red[${round(pred_winnings,2)}]")
-    
-                # running_total = st.session_state.model_pred_running_total
-                # if running_total > 0:
-                #     if pred_winnings > 0:
-                #         color_total = "green"
-                #         color_winnings = "green"
-                #     else:
-                #         color_total = "green"
-                #         color_winnings = "red"
-                # else:
-                #     if pred_winnings <= 0:
-                #         color_total = "red"
-                #         color_winnings = "green"
-                #     else:
-                #         color_total = "red"
-                #         color_winnings = "red"
+                    st.write(f"Model Prediction Running Total: <span style='color:red'>${round(st.session_state.model_pred_running_total,2)}</span>", unsafe_allow_html=True)
 
-                # total_html = f"<span style='color:{color_total}; font-size: 18px'>${round(running_total, 2)}</span>"
-                # winnings_html = f"<span style='color:{color_winnings}; font-size: 12px'>(${round(pred_winnings, 2)})</span>"
-
-                # st.write(f"Model Prediction Running Total: {total_html}  {winnings_html}", unsafe_allow_html=True)
-
-
-
-    except ValueError:
+      except ValueError:
         st.warning("Please enter a valid wager (e.g. 100 or 55.55), excluding the dollar sign.")
 
 
