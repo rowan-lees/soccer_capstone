bc5843a473d689616fcfabcfacebe0399b9a6bec
rowan-lees <rowan-lees@hotmail.com>
2023-06-23 20:20:41 -0700

    .

--- pages/1_💰_Betting_App.py
+++ pages/1_💰_Betting_App.py
@@ -415,9 +415,9 @@ if wager_str:
                     else:
                         color_total = "red"
                         color_winnings = "red"
-
-                total_html = f"<span style='color:{color_total}; font-size: 18px'>${round(running_total, 2)}"
-                winnings_html = f"<span style='color:{color_winnings}; font-size: 12px'>(${round(pred_winnings, 2)})</span></span>"
+                        
+                total_html = f"<span style='color:{color_total}; font-size: 18px'>${round(running_total, 2)}</span>"
+                winnings_html = f"<span style='color:{color_winnings}; font-size: 12px'>(${round(pred_winnings, 2)})</span>"
 
                 st.write(f"Model Prediction Running Total: {total_html}  {winnings_html}", unsafe_allow_html=True)
 
