1b5c936aa34026074249367357480f58a2155b43
rowan-lees <rowan-lees@hotmail.com>
2023-06-23 18:02:04 -0700

    .

--- pages/1_💰_Betting_App.py
+++ pages/1_💰_Betting_App.py
@@ -235,11 +235,21 @@ prediction_scalar = prediction.item()  # Convert prediction to a scalar value
 prediction_labels = {0: "Away Win", 1: "Draw", 2: "Home Win"}
 prediction_text = prediction_labels[prediction_scalar]
 st.markdown(f"Model prediction: {prediction_text}")
-st.markdown(f"Model prediction: {prediction}")
+# st.markdown(f"Model prediction: {prediction}")
 # st.markdown(f"True label: {example_y}"a)
-st.markdown(f'Expected Value of Home Loss {round(EV(prediction_prob[0][0], {samp_a_bet_odds}),3)}')
-st.markdown(f'Expected Value of Draw {round(EV(prediction_prob[0][1], {samp_d_bet_odds}),3)}')
+
+EV_h_win = round(EV(prediction_prob[0][2], {samp_h_bet_odds}),3)
+EV_d_win =round(EV(prediction_prob[0][1], {samp_d_bet_odds}),3)
+EV_a_win =round(EV(prediction_prob[0][0], {samp_a_bet_odds}),3)
+
 st.markdown(f'Expected Value of Home Win {round(EV(prediction_prob[0][2], {samp_h_bet_odds}),3)}')
+st.markdown(f'Expected Value of Draw {round(EV(prediction_prob[0][1], {samp_d_bet_odds}),3)}')
+st.markdown(f'Expected Value of Away Win {round(EV(prediction_prob[0][0], {samp_a_bet_odds}),3)}')
+
+if EV_h_win > 0:
+    st.write(f"Expected Value of Home Win: <span style='color:green'>${EV_h_win}</span>", unsafe_allow_html=True)
+else:
+    st.write(f"Expected Value of Home Win: <span style='color:red'>${EV_h_win}</span>", unsafe_allow_html=True)
 
 
 #betting functionality
